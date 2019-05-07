d3ce22a96cca5b2e1d88715ed92c4445bdc639a2
AlvinGames <kanatush.tim@gmail.com>
2019-05-07 10:50:01 +0300

    animation

--- MultiplayerGame/Book/Utility.hpp
+++ MultiplayerGame/Book/Utility.hpp
@@ -13,6 +13,8 @@ namespace sf
 	class Text;
 }
 
+class Animation;
+
 // Since std::to_string doesn't work on MinGW we have to implement
 // our own to support all platforms.
 template <typename T>
@@ -24,6 +26,7 @@ std::string		toString(sf::Keyboard::Key key);
 // Call setOrigin() with the center of the object
 void			centerOrigin(sf::Sprite& sprite);
 void			centerOrigin(sf::Text& text);
+void			centerOrigin(Animation& animation);
 
 // Degree/radian conversion
 float			toDegree(float radian);
@@ -37,5 +40,5 @@ float			length(sf::Vector2f vector);
 sf::Vector2f	unitVector(sf::Vector2f vector);
 
 
-#include "Utility.inl"
+#include "Book/Utility.inl"
 #endif // BOOK_UTILITY_HPP
