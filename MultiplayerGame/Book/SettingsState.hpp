391d6be3a1a52212faea516165e340eccd335831
AlvinGames <kanatush.tim@gmail.com>
2019-05-09 23:14:35 +0300

    multiplayer settings

--- MultiplayerGame/Book/SettingsState.hpp
+++ MultiplayerGame/Book/SettingsState.hpp
@@ -25,14 +25,14 @@ public:
 
 private:
 	void							updateLabels();
-	void							addButtonLabel(Player::Action action, float y, const std::string& text, Context context);
+	void							addButtonLabel(std::size_t index, std::size_t x, std::size_t y, const std::string& text, Context context);
 
 
 private:
-	sf::Sprite											mBackgroundSprite;
-	GUI::Container										mGUIContainer;
-	std::array<GUI::Button::Ptr, Player::ActionCount>	mBindingButtons;
-	std::array<GUI::Label::Ptr, Player::ActionCount> 	mBindingLabels;
+	sf::Sprite												mBackgroundSprite;
+	GUI::Container											mGUIContainer;
+	std::array<GUI::Button::Ptr, 2 * PlayerAction::Count>	mBindingButtons;
+	std::array<GUI::Label::Ptr, 2 * PlayerAction::Count> 	mBindingLabels;
 };
 
 #endif // BOOK_SETTINGSSTATE_HPP
